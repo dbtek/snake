6177cf724ece29028444b68982f70cf1b535a0ae
İsmail Demirbilek <ce.demirbilek@gmail.com>
2013-12-21 22:37:27 +0200

    snake move operations added. tests added also

--- snake-ng.js
+++ snake-ng.js
@@ -37,16 +37,35 @@ var snakeCtrl = function($scope, squareDrawer){
             {row:6,col:8},
             {row:6,col:9},
             {row:6,col:10}
-        ]
+        ],
+
+        move: function(){
+            console.log(this.body);
+            squareDrawer.clean($scope.grid, this.body.pop());
+            console.log(this.body);
+
+            if(this.direction == 'right'){
+                this.body.splice(0,0,{row: this.body[0].row, col: this.body[0].col+1});
+            }
+            else if(this.direction == 'left'){
+                this.body.splice(0,0,{row: this.body[0].row, col: this.body[0].col-1});
+            }
+            else if(this.direction == 'up'){
+                this.body.splice(0,0,{row: this.body[0].row-1, col: this.body[0].col});
+            }
+            else if(this.direction == 'down'){
+                this.body.splice(0,0,{row: this.body[0].row+1, col: this.body[0].col});
+            }
+        }
     };
 
-    $scope.$watch('walls', function() {
-            console.log('value changed');
-            angular.forEach($scope.walls, function(val, key) {
-                console.log('wall:'+ JSON.stringify(val));
-                squareDrawer.draw($scope.grid, val.head, val.size);
-            });
-    }, true);
+
+    $scope.drawWalls = function(){
+        angular.forEach($scope.walls, function(val, key) {
+            console.log('wall:'+ JSON.stringify(val));
+            squareDrawer.draw($scope.grid, val.head, val.size);
+        });
+    };
 
     $scope.$watch('snake', function() {
         console.log('snake changed');
@@ -56,6 +75,7 @@ var snakeCtrl = function($scope, squareDrawer){
         });
     }, true);
 
+    $scope.drawWalls();
 
 };
 
@@ -82,6 +102,20 @@ snakeApp.factory('squareDrawer', function() {
 
                 ctx.clearRect((head.col - 1) * boxWidth, (head.row - 1) * boxHeight, size.width * boxWidth, size.height * boxHeight);
                 ctx.fillRect((head.col - 1) * boxWidth, (head.row - 1) * boxHeight, size.width * boxWidth, size.height * boxHeight);
+            }
+        },
+
+        clean: function(grid, box){
+            var canvas = document.getElementById("snakeGameCanvas");
+
+            boxWidth = canvas.width / grid.cols;
+            boxHeight = canvas.height / grid.rows;
+
+
+            if (canvas.getContext) {
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect((box.col - 1) * boxWidth, (box.row - 1) * boxHeight, 1 * boxWidth, 1 * boxHeight);
+                console.log('box cleaned');
             }
         }
     };
